132e9fa39c3ba140558f4b9ad0570ceceddbb7d6
asuszsg <1724473740@qq.com>
2019-07-01 23:23:22 +0800

    ssm 配置

---
+++ manager_web/src/test/java/com/hkd/test/ManagerTest.java
@@ -0,0 +1,31 @@
+package com.hkd.test;
+
+import com.hkd.mapper.UsersMapper;
+import com.hkd.pojo.Users;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Author: 张世罡
+ * @Date： 2019/7/1
+ * @Description: com.hkd.test
+ * @version: 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/applicationContext-dao.xml")
+public class ManagerTest {
+
+    @Autowired
+    private UsersMapper usersMapper;
+
+    @Test
+    public void insertUserTest(){
+        Users users=new Users();
+        users.setUserage(20);
+        users.setUsername("admin");
+        this.usersMapper.insertUsers(users);
+    }
+}
